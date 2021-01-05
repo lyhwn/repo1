a8d64583459c945e29ef7d9996667b0bb96a7260
hrh <1040542203@qq.com>
2021-01-05 22:00:13 +0800

    animal,cat,main

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -3,6 +3,13 @@ import org.junit.Test;
 public class Main {
 
     @Test
+    public void dome1() {
+
+        Animal a=new Cat();
+        a.eat();
+    }
+
+    @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
