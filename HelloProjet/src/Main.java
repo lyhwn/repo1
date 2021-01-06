69d14e7e0c18c207f3d141ac1f3bd50d53aebe13
hrh <1040542203@qq.com>
2021-01-07 00:38:38 +0800

    demo2

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -1,12 +1,33 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class Main {
 
     @Test
-    public void dome1() {
+    public void demo1() {
 
-        Animal a=new Cat();
-        a.eat();
+        String aaa="aaa";
+        String bbb="bbbb";
+        System.out.println(aaa.compareTo(bbb));
+
+
+
+    }
+
+    public void demo2(){
+        String inoutStr="sdfkjsdl1dsrfe54s2dfd1w3145e";
+        for (char c:inoutStr.toCharArray()){
+            if ("1234567890".contains(c+"")){
+                System.out.print(c);
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < inoutStr.length(); i++) {
+            if ("1234567890".contains(inoutStr.charAt(i)+"")){
+                System.out.print(inoutStr.charAt(i));
+            }
+        }
     }
 
     @Test
